dc027b33b08309753c4bd7524973ed1b583192b7
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-08 14:55:58 -0500

    Implement filters and qualifications for candidate selection

--- filters.rb
+++ filters.rb
@@ -1,16 +1,28 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+# Takes in an id and finds a candidate with that id, if not found, returns nil
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
+end
+
+# Returns true if candidate has 2 years of eperience or more and returns false otherwise
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+# Takes in a collection of candidates and returns a subset that meet specific criteria
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+    candidate[:date_applied] >= 15.day.ago.to_date &&
+    candidate[:age] >= 18
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+# Takes in a collection of candidates and returns them reordered by highest to lowest qualifications
+def ordered_by_qualifications (candidates)
+  candidates.sort_by! { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
--- main.rb
+++ main.rb
@@ -4,6 +4,21 @@ require 'pp'
 require './candidates'
 require './filters'
 
-## Your test code can go here
+## Test code for your methods
 
+# Find a candidate by ID
+puts "\nTesting find method with ID 11:"
+candidate = find(11)
+pp candidate
+
+# Get qualified candidates
+puts "\nTesting qualified_candidates method:"
+pp qualified_candidates(@candidates)
+
+# Order candidates by quaifications
+puts "\nTesting ordered_by_qualifications method:"
+pp ordered_by_qualifications(@candidates)
+
+# Pretty print the candidates array
+puts "\nAll Candidates:"
 pp @candidates
